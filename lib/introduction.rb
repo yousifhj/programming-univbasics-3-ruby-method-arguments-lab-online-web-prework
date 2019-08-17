97a898dc95bd7248862017c4835b67451f5dbf21
Huda Yousif <hjdoski@gmail.com>
2019-08-17 18:48:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,8 +6,8 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional
-  puts 
+def introduction_with_language_optional(name,language,ruby)
+  puts  "Hi, my name is #{name} and I am learning to program in #{language}."
 
 
 
